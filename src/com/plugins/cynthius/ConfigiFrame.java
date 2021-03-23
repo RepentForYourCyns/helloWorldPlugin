399d90d1c471e3f95ee4c4e4afcdf269df08ea1d
RepentForYourCyns <cosmojoe@hotmail.co.uk>
2021-03-23 20:38:02 +0000

    Added config and EntitySpawnEvent listener

--- src/com/plugins/cynthius/ConfigiFrame.java
+++ src/com/plugins/cynthius/ConfigiFrame.java
@@ -1,8 +1,14 @@
 package com.plugins.cynthius;
 
 import java.util.List;
+
 import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
@@ -10,10 +16,26 @@ import org.bukkit.plugin.java.JavaPlugin;
  * Plugin to enable configuration of noDamageTicks
  * 
  * @author Cosmo De Bonis-Campbell
- *
  */
-public class ConfigiFrame extends JavaPlugin {
+public class ConfigiFrame extends JavaPlugin implements Listener {
 	
+	int maxNoDamageTicks;
+	
+	@EventHandler
+	public void onEntitySpawn(EntitySpawnEvent event) {
+		Entity e = event.getEntity();
+		if(event instanceof LivingEntity) {
+			LivingEntity l = ((LivingEntity) e);
+			set(maxNoDamageTicks, l);
+		}
+	}
+	
+	@Override
+	public void onLoad() {
+		this.saveDefaultConfig();
+		super.onLoad();
+	}
+
 	@Override	
 	public void onDisable() {
 		getLogger().info("[ConfigiFrame shut down]");
@@ -22,14 +44,25 @@ public class ConfigiFrame extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+		maxNoDamageTicks = getConfig().getInt("maximum-no-damage-ticks");
+		set(maxNoDamageTicks);
 		getLogger().info("[ConfigiFrame launched]");
 		super.onEnable();
 	}
 	
-	public void noDamageTicks() {
-		List<LivingEntity> list = Bukkit.getServer().getWorld("world").getLivingEntities();
-		for(LivingEntity e : list) {
-			e.setNoDamageTicks(0);
+	public static void set(int ticks) {
+		List<World> worlds = Bukkit.getServer().getWorlds();
+		for(World w : worlds) {
+			String name = w.getName();
+			List<LivingEntity> livingEntities = Bukkit.getServer().getWorld(name).getLivingEntities();
+			for(LivingEntity e : livingEntities) {
+				e.setMaximumNoDamageTicks(ticks);
+			}
 		}
 	}
-}
+	
+	public static void set(int ticks, LivingEntity e) {
+				e.setMaximumNoDamageTicks(ticks);
+	}
+}		
